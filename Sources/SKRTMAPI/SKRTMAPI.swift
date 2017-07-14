927148b1f4c074b1f4ac58985211264644deff2a
Peter Zignego <peter.zignego@smashingboxes.com>
2017-07-14 16:30:35 -0400

    rtm.connect

--- Sources/SKRTMAPI/SKRTMAPI.swift
+++ Sources/SKRTMAPI/SKRTMAPI.swift
@@ -73,21 +73,37 @@ public final class SKRTMAPI: RTMDelegate {
         self.rtm.delegate = self
     }
 
-    public func connect() {
-        WebAPI.rtmStart(
-            token: token,
-            simpleLatest: options.simpleLatest,
-            noUnreads: options.noUnreads,
-            mpimAware: options.mpimAware,
-            success: {(response) in
-            guard let socketURL = response["url"] as? String, let url = URL(string: socketURL) else {
-                return
-            }
-            self.rtm.connect(url: url)
-            self.adapter?.initialSetup(json: response, instance: self)
-        }, failure: { (error) in
-            print(error)
-        })
+    public func connect(withInfo: Bool = true) {
+        if withInfo {
+            WebAPI.rtmStart(
+                token: token,
+                simpleLatest: options.simpleLatest,
+                noUnreads: options.noUnreads,
+                mpimAware: options.mpimAware,
+                success: {(response) in
+                    guard let socketURL = response["url"] as? String, let url = URL(string: socketURL) else {
+                        return
+                    }
+                    self.rtm.connect(url: url)
+                    self.adapter?.initialSetup(json: response, instance: self)
+                }, failure: { (error) in
+                    print(error)
+            })
+        } else {
+            WebAPI.rtmConnect(
+                token: token,
+                batchPresenceAware: options.batchPresenceAware,
+                presenceSub: options.presenceSub,
+                success: {(response) in
+                    guard let socketURL = response["url"] as? String, let url = URL(string: socketURL) else {
+                        return
+                    }
+                    self.rtm.connect(url: url)
+                    self.adapter?.initialSetup(json: response, instance: self)
+                }, failure: { (error) in
+                    print(error)
+            })
+        }
     }
 
     public func disconnect() {
