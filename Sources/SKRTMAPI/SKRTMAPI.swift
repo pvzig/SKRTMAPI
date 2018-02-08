d1a011a2cc77ad7a366df5324a88fa4764bc189a
Peter Zignego <pzignego@gmail.com>
2018-02-07 22:56:43 -0500

    Revert change

--- Sources/SKRTMAPI/SKRTMAPI.swift
+++ Sources/SKRTMAPI/SKRTMAPI.swift
@@ -21,7 +21,9 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#if os(Linux)
 import Dispatch
+#endif
 import Foundation
 import SKWebAPI
 @_exported import SKCore
